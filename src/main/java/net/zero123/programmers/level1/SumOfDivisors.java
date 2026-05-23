62ac94a055d0b08d586fff10c68c9ea0f2c4af5a
JAEMO-LIM <tjdwkddl@gmail.com>
2026-05-23 15:53:38 +0900

    약수의 합 구하기

---
+++ src/main/java/net/zero123/programmers/level1/SumOfDivisors.java
@@ -0,0 +1,16 @@
+package net.zero123.programmers.level1;
+
+public class SumOfDivisors {
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i=n;i>0;i--) {
+            if(n%i==0) {
+                answer+=i;
+            }
+        }
+
+
+        return answer;
+    }
+}
