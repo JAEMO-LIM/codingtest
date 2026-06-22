60e8447752108ac22489ded36ed93278896eaeaa
JAEMO-LIM <tjdwkddl@gmail.com>
2026-06-22 20:52:20 +0900

    P1546 평균 구하기 Remind

--- src/main/java/net/zero123/doitalgoritm/P1546.java
+++ src/main/java/net/zero123/doitalgoritm/P1546.java
@@ -7,23 +7,21 @@ public class P1546 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int max = 0;
-        double sum =0;
+        double m =0 ;
+        double sum = 0;
 
-        int arr[] = new int[n];
-
+        double arr[] = new double[n];
         for(int i=0;i<n;i++) {
             arr[i] = sc.nextInt();
-            if(max < arr[i]) {
-                max = arr[i];
+            if(m < arr[i]) {
+                m = arr[i];
             }
         }
 
         for(int i=0;i<n;i++) {
-            sum += (double) arr[i]/max*100;
+            sum += arr[i]/m*100;
         }
 
         System.out.println(sum/n);
-
     }
 }
