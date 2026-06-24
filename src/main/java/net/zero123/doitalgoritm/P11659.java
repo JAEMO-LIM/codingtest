bb37ac9025724f8c28293bddaabee2b089d32e4c
JAEMO-LIM <tjdwkddl@gmail.com>
2026-06-24 19:30:47 +0900

    P11659 구간합 구하기, StringTokenizer, BufferedReader

---
+++ src/main/java/net/zero123/doitalgoritm/P11659.java
@@ -0,0 +1,31 @@
+package net.zero123.doitalgoritm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int sumArr[] = new int[n+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=1;i<n+1;i++) {
+            sumArr[i] = sumArr[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0;i<m;i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            System.out.println(sumArr[b] - sumArr[a-1]);
+        }
+
+    }
+}
