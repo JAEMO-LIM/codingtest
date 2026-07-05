9e61af2e4f89cc7461566ee6a49eba943e76a24a
JAEMO-LIM <tjdwkddl@gmail.com>
2026-07-05 17:00:15 +0900

    P17298 오큰수 구하기

---
+++ src/main/java/net/zero123/doitalgoritm/P17298.java
@@ -0,0 +1,35 @@
+package net.zero123.doitalgoritm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class P17298 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int [n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer[] = {-1,-1,-1,-1};
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<n;i++) {
+            while(!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                answer[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        for(int i=0;i<n;i++) {
+            System.out.print(answer[i] + " ");
+        }
+
+    }
+}
