e65af957d34b3cde58b26e52e8fd8bab08970c44
JAEMO-LIM <tjdwkddl@gmail.com>
2026-07-05 15:45:20 +0900

    P1874 Review

--- src/main/java/net/zero123/doitalgoritm/P1874.java
+++ src/main/java/net/zero123/doitalgoritm/P1874.java
@@ -8,33 +8,37 @@ public class P1874 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
         int a = 1;
         int arr[] = new int[n];
 
         for(int i=0;i<n;i++) {
             arr[i] = sc.nextInt();
         }
+        Stack<Integer> st = new Stack<>();
 
-        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
 
         for(int i=0;i<n;i++) {
-            if(a <= arr[i]) {
-                while(a <= arr[i]) {
-                    System.out.println("+");
-                    stack.push(a);
+            int target = arr[i];
+            if(a <= target) {
+                while(a<=target) {
+                    st.push(a);
                     a++;
+                    sb.append("+\n");
                 }
-                System.out.println("-");
-                stack.pop();
-            } else {
-                if(stack.peek() == arr[i]) {
-                    stack.pop();
-                    System.out.println("-");
-                } else {
+                st.pop();
+                sb.append("-\n");
+            }else {
+                if(st.peek() == target) {
+                    st.pop();
+                    sb.append("-\n");
+                }else {
                     System.out.println("NO");
                     return;
                 }
             }
         }
+        System.out.println(sb);
     }
 }
