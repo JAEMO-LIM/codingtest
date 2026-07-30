4b3071de318a2a73fc2fe519c70e582a54ea4cdb
JAEMO-LIM <tjdwkddl@gmail.com>
2026-07-30 22:01:36 +0900

    P11004 QuickSort 구현

---
+++ src/main/java/net/zero123/doitalgoritm/Sort/P11004.java
@@ -0,0 +1,52 @@
+package net.zero123.doitalgoritm.Sort;
+
+import java.util.Scanner;
+
+public class P11004 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int arr[] = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        quickSort(arr,0,n-1);
+
+        System.out.println(arr[m-1]);
+
+
+    }
+
+    public static void quickSort(int[] arr, int low,int high) {
+        if (low < high) {
+            int pivot_index = partition(arr, low, high);
+
+            quickSort(arr, low, pivot_index - 1);
+            quickSort(arr, pivot_index + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr,int low, int high) {
+
+            int pivot = arr[high];
+            int i = low -1;
+
+            for(int j=low;j<high;j++) {
+                if(arr[j]<=pivot) {
+                    i++;
+                    swap(arr,i,j);
+                }
+            }
+            swap(arr, i+1, high);
+            return i+1;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
